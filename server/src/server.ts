782da37da72120a078a2f412191a0ebdc30fbf10
Caique Ribeiro de Oliveira <caique@usekojo.com>
2023-05-20 14:44:57 -0300

    PORT

--- server/src/server.ts
+++ server/src/server.ts
@@ -11,7 +11,13 @@ import { resolve } from 'node:path'
 
 const app = fastify()
 
+const PORT = Number(process.env.PORT) || 3333
 app.register(multipart)
+
+app.addHook('onRequest', (request, reply, done) => {
+  console.log(`${request.method} ${request.url}`)
+  done()
+})
 
 app.register(require('@fastify/static'), {
   root: resolve(__dirname, '../uploads'),
@@ -32,9 +38,9 @@ app.register(memoriesRoutes)
 
 app
   .listen({
-    port: 3333,
+    port: PORT,
     host: '0.0.0.0',
   })
   .then(() => {
-    console.log('🚀 HTTP server running on port http://localhost:3333')
+    console.log(`🚀 HTTP server running on port http://localhost:${PORT}`)
   })
